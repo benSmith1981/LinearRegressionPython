2ee6861168ec3cb761bbb68390619f412c38b795
Ben Smith <benjaminsmith1981@googlemail.com>
2022-10-06 11:21:57 +0000

    predicting data not plotting it

---
+++ linearReg.py
@@ -0,0 +1,33 @@
+import pandas
+import sklearn
+# importing the required module
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+df = pandas.read_csv("data.csv")
+
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+#predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
+predictedCO2 = regr.predict([[2000, 1428]])
+
+
+print(predictedCO2)
+
+# plotting the points 
+plt.plot(X, y)
+  
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+  
+# giving a title to my graph
+plt.title('My first graph!')
+  
+# function to show the plot
+plt.show()
